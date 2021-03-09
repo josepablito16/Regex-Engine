7d13c93d13d3e85379c7286d95b0b6f383e7ccbb
josepablito16 <josepablocif16@gmail.com>
2021-03-08 18:59:10 -0600

    Implementacion de construccion del Kleen Star NFA

--- Entrega final/Thompson.py
+++ Entrega final/Thompson.py
@@ -11,9 +11,11 @@ def construirUnSimbolo(simbolo):
     
 
 def construirOr(NFA1, NFA2):
+    # Se crea el NFA y su estado inicial
     NFA = {}
     NFA[1] = NodoThompson(True, False)
 
+    # Se actualizan los ids de los NFAs
     idContador = 1
     NFA1, idContador = actualizarIdsNodos(NFA1, idContador)
 
@@ -30,6 +32,7 @@ def construirOr(NFA1, NFA2):
     idInicial2, NFA2 = getIdInicial(NFA2)
     idFinal2, NFA2 = getIdFinal(NFA2)
     
+    # Join de NFAs
     NFA.update(NFA2)
     NFA.update(NFA1)
     print(f"NFA = {NFA}")
@@ -50,6 +53,36 @@ def construirOr(NFA1, NFA2):
     return NFA
 
     
+def construirKleen(NFA1):
+    # Se crea el NFA y su estado inicial
+    NFA = {}
+    NFA[1] = NodoThompson(True, False)
+
+    # Se actualizan los ids de los NFAs
+    idContador = 1
+    NFA1, idContador = actualizarIdsNodos(NFA1, idContador)
+
+    # Se obtiene el estado inicial y final de
+    # NFA1 y tambien se quitan los estados locales
+    idInicial1, NFA1 = getIdInicial(NFA1)
+    idFinal1, NFA1 = getIdFinal(NFA1)
+
+    # Join de NFAs
+    NFA.update(NFA1)
+
+    # Se crea el nodo final
+    idContador += 1
+    NFA[idContador] = NodoThompson(False, True)
+
+    # Se crean las relaciones del nodo inicial
+    NFA[1].agregarRelacion(RelacionThompson(1, "ε", idInicial1))
+    NFA[1].agregarRelacion(RelacionThompson(1, "ε", idContador))
+
+    # Se agregan relaciones del NFA1
+    NFA[idFinal1].agregarRelacion(RelacionThompson(idFinal1, "ε", idInicial1))
+    NFA[idFinal1].agregarRelacion(RelacionThompson(idFinal1, "ε", idContador))
+
+    return NFA
 
 
 
@@ -62,4 +95,6 @@ if __name__ == '__main__':
     NFA2 = construirUnSimbolo("b")
     
     NFAOR = construirOr(NFA1, NFA2)
-    visualizarNFA(NFAOR)
+
+    NFAKleen = construirKleen(NFAOR)
+    visualizarNFA(NFAKleen)

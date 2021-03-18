0aefbb556c7e1e7eb8b64a19e106e72127ada762
josepablito16 <josepablocif16@gmail.com>
2021-03-18 00:17:37 -0600

    Se dejan comentadas todas las expresiones de la presentacion final en el main

--- Entrega final/Main.py
+++ Entrega final/Main.py
@@ -9,21 +9,31 @@ a = Arbol()
 #entrada = "(a|b)*abb"
 #entrada = "(((a.((b.a).((b.a)*α)))|((a.(b*α)).a))|(a|b))"
 '''
+(a|b)*a(a|b)(a|b)+  =   (((((a|b)*α).a).(a|b)).(((a|b)*α).(a|b)))
 ((1?)*)*    =   (((1|ε)*α)*α)
+(a|ε)b(a+)c?    =   ((((a|ε).b).((a*α).a)).(c|ε))
+(1|0)+001   =   ((((((1|0)*α).(1|0)).0).0).1)
+(εa|εb)*abb =   ((((((ε.a)|(ε.b))*α).a).b).b)
 '''
-entrada = "(((1|ε)*α)*α)"
+entrada = "((((((ε.a)|(ε.b))*α).a).b).b)"
+
+
+
+
+
+
 
 
 cadena = "1"
 
 
 NFA = a.interpretarEcuacion(entrada)
-#g.visualizarNFA(NFA)
+g.visualizarNFA(NFA)
 g.visualizarNFD(s.generarSubConjuntos(NFA))
 
 
 # DFA directo
 entrada = f"({entrada}.#)"
-#g.visualizarDirecto(a.armarArbol(entrada))
+g.visualizarDirecto(a.armarArbol(entrada))
 
 
